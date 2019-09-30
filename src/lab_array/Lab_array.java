989b0c84f53cd3295fb0b824292863d1e56824a4
AKHEEM <AKHEEM@Halil-laptop1>
2019-09-30 16:11:38 +0800

    Commit Pertama

---
+++ src/lab_array/Lab_array.java
@@ -0,0 +1,19 @@
+package lab_array;
+//@author AKHEEM
+
+import java.util.Scanner;
+
+public class Lab_array {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in); 
+        String[] namaBulan = {"Januari", "Februari", "Mac", "April", "Mei", "Jun", 
+                              "Julai", "Ogos", "September", "Oktober", "November", "Disember"}; 
+        System.out.println("NAMA-NAMA BULAN DALAM KALENDAR"); 
+        System.out.print("Masukkan bulan kelahiran anda (1 hingga 12): "); 
+        int bilBulan = input.nextInt(); 
+         
+        System.out.println("Bulan " +bilBulan+ " adalah bulan " +namaBulan[bilBulan-1]); 
+         
+        } 
+    } 
